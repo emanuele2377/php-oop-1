b8918516ae07c68c77bcd3b8008782680d8cd1b7
Your Name <you@example.com>
2024-01-12 17:05:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+class Movie
+{
+
+    function __construct($_durata)
+    {
+    }
+}
+
+$oppenheimer = new Movie($_durata);
+$disturbia = new Movie($_durata);
+$casino = new Movie($_durata);
+$alien = new Movie($_durata);
